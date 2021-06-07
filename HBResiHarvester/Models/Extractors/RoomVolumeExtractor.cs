fa713e3b81c926f19e3711ea3b631013106738c2
AzureAD\NicholasRawitscher <nicholas@bimorph.com>
2021-06-07 18:23:10 +0100

    Added IGeometryExtractor

--- HBResiHarvester/Models/Extractors/RoomVolumeExtractor.cs
+++ HBResiHarvester/Models/Extractors/RoomVolumeExtractor.cs
@@ -3,10 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
 
-namespace HBResiHarvester.Models.Extractors
+namespace HBResiHarvester.Extractors
 {
-    class RoomVolumeExtractor
+    /// <summary>
+    /// Extracts the shells of all the <see cref="Room"/>'s
+    /// in the current Revit document.
+    /// </summary>
+    internal class RoomShellExtractor : IGeometryExtractor<Solid>
     {
+        private readonly IEnumerable<Room> _rooms;
+
+        /// <summary>
+        /// Construct a <see cref="RoomVolumeExtractor"/>.
+        /// </summary>
+        internal RoomShellExtractor(IEnumerable<Room> rooms) => _rooms = rooms;
+
+        public IEnumerable<Solid> Extract()
+        {
+            var solids = new List<Solid>();
+
+            foreach (var room in _rooms)
+            {
+
+                var shell = room.ClosedShell;
+
+                foreach (var geoObject in shell)
+                {
+                    var geoObjectInstance = geoObject as GeometryInstance;
+
+                    if (null == geoObjectInstance) continue;
+
+                    foreach (var instObj in geoObjectInstance.SymbolGeometry)
+                    {
+                        var solid = instObj as Solid;
+                        if (null == solid ||
+                            0 == solid.Faces.Size ||
+                            0 == solid.Edges.Size) continue;
+
+                        solids.Add(solid);
+
+                    }
+                }
+            }
+        }
+       
     }
 }
---
+++ HBResiHarvester/Models/Interfaces/IGeometryExtractor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HBResiHarvester.Interfaces
+{
+    /// <summary>
+    /// A contract which all concrete implementations should implement
+    /// in order to extract geometrical data from
+    /// an  entity that contains geometrical attributes.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IGeometryExtractor<out T>
+    {
+        /// <summary>
+        /// Extracts a collection of a defined geometrical information
+        /// from an entity that contains geometrical attributes.
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<T> Extract();
+    }
+}
